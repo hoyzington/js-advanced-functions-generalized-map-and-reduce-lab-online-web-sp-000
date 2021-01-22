3b9755b8953123899a9d82efc19c55fff9d09a04
Scott Hoisington <hoyzington@gmail.com>
2021-01-22 03:31:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,12 +6,22 @@ function map(array, func) {
   return result;
 }
 
+<<<<<<< HEAD
 
+=======
+<<<<<<< HEAD
+>>>>>>> 8953ce7b1e1016b4026e30f159fd62247037bbe0
 function reduce(source, func, start){
   let result = !!start ? start : source[0];
   let array = !!start ? source : source.slice(1);
   for (const item of array) {
     result = func(item, result);
+=======
+function reduce(array, func, start=0) {
+  let result = start;
+  for (const item of array) {
+    result += func(item);
+>>>>>>> 7d50e1ee7ba892711430f6caeee42833eb9bf0a4
   }
   return result;
 }
